195a39ae8310bbafb6e9977d3cfb076af0916e8e
aditik4 <aditik4@illinois.edu>
2022-12-06 21:35:07 -0600

    obtains tweets given id

--- src/Data/tweetscript.py
+++ src/Data/tweetscript.py
@@ -1,19 +1,64 @@
-import tweepy
- 
-# API keyws that yous saved earlier
-api_key = "1dWU3QayflekgFSnmnTur7841"
-api_secrets = "Uqpty3X7zCQYgJ1ariHOkd2FS8xOaUadQ1S4TNGvJE8Rvd6gsA"
-access_token = "1596710325063241730-Yo9eH11mWUz88aVZzeiIdOwFHfC67e"
-access_secret = "OKxmWHX4kRb7YNhp9EUU3RcaRNKNqCX291QqO1wBSQClo"
- 
-# Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(api_key,api_secrets,access_token,access_secret)
-#auth.set_access_token(access_token,access_secret)
+import requests
+import os
+import json
 
-api = tweepy.API(auth)
- 
-try:
-    api.verify_credentials()
-    print('Successful Authentication')
-except:
-    print('Failed authentication')
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAN0ejwEAAAAAG6RofdC3LcMq4iTMeUG%2F2MDWPa4%3DtEZKKeXijG7h4f9BUfdJrMrZy9zKIFAjlrqUldJkZ3VbOrNrYZ"
+
+id_list = []
+def getIds():
+    file = open("ids.txt", "r")
+    for sentence in file:
+        splitted = sentence.split(",")
+        id = splitted[0]
+        id_list.append(id)
+    #print(id_list)
+
+
+def create_url():
+    tweet_fields = "tweet.fields=lang,author_id"
+    getIds()
+    subset = id_list[0:99]
+    print("subsert")
+    print(subset)
+    ids = ",".join(subset)
+    idparam = "ids=" + ids
+    print(ids)
+    #ids = "ids=1278747501642657792,1255542774432063488"
+    # You can adjust ids to include a single Tweets.
+    # Or you can add to up to 100 comma-separated IDs
+    url = "https://api.twitter.com/2/tweets?{}&{}".format(idparam, tweet_fields)
+    return url
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2TweetLookupPython"
+    return r
+
+
+def connect_to_endpoint(url):
+    response = requests.request("GET", url, auth=bearer_oauth)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+
+def main():
+    url = create_url()
+    json_response = connect_to_endpoint(url)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
