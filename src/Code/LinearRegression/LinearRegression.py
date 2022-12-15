5a57b87634324c2b5813cb876ca9d721d885d89d
erikly <erikly2@illinois.edu>
2022-12-15 16:52:02 -0600

    linear fixed

--- src/Code/LinearRegression/LinearRegression.py
+++ src/Code/LinearRegression/LinearRegression.py
@@ -6,15 +6,30 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 X = []
-y = [1,1,1,1,1]
+y = []
 i = 0
 file1 = open("src/Data/hatespeech.txt", 'r', encoding="utf8")
 lines = file1.readlines()
-for line in lines:
-    for word in line:
-        if word == "asian":
+file2 = open("src/Data/ids.txt", 'r', encoding="utf8")
+id_lines = file2.readlines()
+for i, h_line in enumerate(lines):
+    if h_line == 'no data\n':
+        continue
+    X.append(0)
+    res = h_line.split()
+    for word in res:
+        print(word)
+        if word == "Asian":
             X[i]+=1
-    i+=1
+    #i+=1
+    if id_lines[i].__contains__("racism"):
+        y.append(0.8)
+    elif id_lines[i].__contains__("sexism"):
+        y.append(0.7)
+    else:
+        y.append(0)
+print(X)
+print(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 model_fit = LinearRegression()
