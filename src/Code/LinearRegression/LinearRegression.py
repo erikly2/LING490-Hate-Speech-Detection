1b87478b3633ee541b43684131c0188e9aa2b7fe
erikly <erikly2@illinois.edu>
2022-12-15 17:28:04 -0600

    sexism

--- src/Code/LinearRegression/LinearRegression.py
+++ src/Code/LinearRegression/LinearRegression.py
@@ -5,23 +5,24 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
 X = []
 y = []
-i = 0
 file1 = open("src/Data/hatespeech.txt", 'r', encoding="utf8")
 lines = file1.readlines()
 file2 = open("src/Data/ids.txt", 'r', encoding="utf8")
 id_lines = file2.readlines()
 for i, h_line in enumerate(lines):
-    if h_line == 'no data\n':
-        continue
     X.append(0)
+    if h_line == 'no data\n':
+        y.append(0)
+        continue
     res = h_line.split()
     for word in res:
-        print(word)
-        if word == "Asian":
+        if word == "Asian" or word == "serbia" or word == "Serbian" or word == "Drasko":
             X[i]+=1
-    #i+=1
+        elif word == "bitches" or word == "pimping" or word == "trashy" or word == "girls":
+            X[i]+=1
     if id_lines[i].__contains__("racism"):
         y.append(0.8)
     elif id_lines[i].__contains__("sexism"):
